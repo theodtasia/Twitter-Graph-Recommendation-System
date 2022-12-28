6248001bd07d3fd7952f582c156d69307f73e58f
xristina <ckaralka@csd.auth.gr>
2022-12-28 08:04:43 +0200

    dataloader incorrect split

--- recommendation_task/graph_dataset.py
+++ recommendation_task/graph_dataset.py
@@ -2,16 +2,16 @@ import pickle
 from random import shuffle
 
 import torch
-from torch_geometric.utils import negative_sampling
-
 from clean_datasets import clean_data_path, Graph_
 from features_extraction import FeaturesExtraction
 from torch_geometric.data import Data
 from torch_geometric.transforms import RandomLinkSplit
+from torch_geometric.loader import LinkNeighborLoader
 
 
 clean_data_path = f'../{clean_data_path}'
 device = 'cpu'
+
 class Dataset:
 
     def __init__(self):
@@ -34,34 +34,81 @@ class Dataset:
     def get_day_graph_edge_index(self, day):
         # get undirected edges of graph_day as tensor
         edges = pickle.load(open(f'{clean_data_path}{Graph_}{day}', 'rb')).edges()
+        return self.to_edge_index(edges)
+
+    def to_edge_index(self, edges):
         edges = torch.tensor(list(edges), device=device, dtype=torch.long).T
-        edges = self.turn_to_undirected(edges)
+        edges = self.to_undirected(edges)
         return edges
 
-    def turn_to_undirected(self, edge_index):
+    def to_undirected(self, edge_index):
         return torch.cat([edge_index, edge_index[[1, 0]]], dim=1)
 
 
     def split_edges(self, msg_pass_ratio=.5):
         transform = RandomLinkSplit(is_undirected=True, num_val= 1 - msg_pass_ratio, num_test=0,
                                     add_negative_train_samples=False, neg_sampling_ratio=0)
-        msg_pass, supervision, _ = (split.edge_label_index for split in transform(self.graph))
-        # print(self.graph.edge_index)
-        # print(msg_pass, '\n', supervision)
-        self.negative_sampling(self.graph.edge_index, msg_pass, supervision)
+        msg_pass, supervision, _ = (self.to_undirected(split.edge_label_index)
+                                    for split in transform(self.graph))
+        print(msg_pass, '\n', supervision)
+        neg = self.negative_sampling(msg_pass, supervision)
+        print(neg)
 
-    def negative_sampling(self, init, msg_pass, supervision):
-        nodes = list(set(v.item() for v in init.view(-1)))
-        print(nodes)
+        edge_label = torch.cat([
+            torch.full((edge_set.shape[1], ), edge_label, device=device, dtype=torch.int8)
+            for edge_set, edge_label in [(msg_pass, 0), (supervision, 1), (neg, 2)]
+        ])
+        print(edge_label)
+        self.graph.edge_index = torch.cat([msg_pass, supervision, neg], dim=1)
+        self.graph.edge_label = edge_label
+        print(self.graph)
+        print(self.graph.edge_index)
+
+
+
+    def negative_sampling(self, msg_pass, supervision):
+        nodes = list(set(v.item() for v in self.graph.edge_index.view(-1)))
+        msg_pass_edges = set((v,u) for v, u in zip(msg_pass[0], msg_pass[1]))
+        negative_samples = set()
         for v in supervision[0]:
+            v = v.item()
             shuffle(nodes)
-            neg = None
             for u in nodes:
-                if not ()
+                if u > v and (v, u) not in msg_pass_edges and (u,v) not in negative_samples:
+                    negative_samples.add((v, u))
+                    break
+
+        return self.to_edge_index(negative_samples)
+
+    @staticmethod
+    def edge_subsets(batch):
+        edge_sets = (
+            torch.stack([
+                batch.edge_index[i][((batch.edge_label == label).nonzero(as_tuple=True)[0])]
+                for i in range(2)])
+            for label in range(3)
+        )
+        for t in edge_sets:
+            print(t)
+        return edge_sets
+
+
+    def dataloader(self, batch_size=4):
+        return LinkNeighborLoader(
+            self.graph,
+            # Sample 30 neighbors for each node for 2 iterations
+            num_neighbors=[30] * 2,
+            # Use a batch size of _ for sampling training nodes
+            batch_size=batch_size,
+            edge_label_index=self.graph.edge_index,
+            edge_label=self.graph.edge_label
+        )
 
 
 
-
-
-
-Dataset().split_edges()
+ds = Dataset()
+ds.split_edges()
+Dataset.edge_subsets(ds.graph)
+loader = ds.dataloader()
+for batch in loader:
+    print(batch.edge_label)
