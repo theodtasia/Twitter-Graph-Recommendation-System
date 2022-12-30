34c77ae278e28f8c5ca34895bad1f766403b89b4
theodtasia <theodtasia@csd.auth.gr>
2022-12-31 00:41:05 +0200

    remove similarities

--- recommendation_task/test_model.py
+++ recommendation_task/test_model.py
@@ -1,6 +1,11 @@
+import torch
+
 from utils import set_seed, device
 from recommendation_task.train_model import TrainClassificationModel
 from recommendation_task.graph_dataset import Dataset
+from sklearn.metrics import roc_auc_score
+import tqdm
+
 
 class TestClassificationModel:
 
@@ -11,7 +16,16 @@ class TestClassificationModel:
         self.dataset = Dataset()
         self.model = TrainClassificationModel()
         self.model.to(self.device)
-        self.test_model()
+        # self.test_model()
 
 
+    def __getTraining(self):
+        sampled_data = next(iter(self.dataset))
+        preds = []
+        ground_truths = []
+        pred = torch.cat(preds, dim=0).cpu().numpy()
+        ground_truth = torch.cat(ground_truths, dim=0).cpu().numpy()
+        auc = roc_auc_score(ground_truth, pred)
+        print()
+        print(f"Validation AUC: {auc:.4f}")
 
